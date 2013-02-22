795bfb87f39bddce95f13d440b426617f900089b
ehoogerbeets1 <ehoogerbeets1@5ac057f5-ce63-4fb3-acd1-ab13b794ca36>
2013-02-22 20:28:45 +0000

    Was missing from source control. This controls what is built into the unit test copy of ilib
    git-svn-id: svn+ssh://svn.code.sf.net/p/i18nlib/code/trunk@38 5ac057f5-ce63-4fb3-acd1-ab13b794ca36

---
+++ js/src/ilib-ut-inc.js
@@ -0,0 +1,65 @@
+/**
+ * @license
+ * Copyright © 2012, JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * ilib.js - metafile that includes all other js files
+ */
+
+/* !depends
+ilibglobal.js
+daterangefmt.js
+date.js
+calendar/hebrewdate.js
+calendar/hebrew.js
+calendar/islamic.js
+calendar/islamicdate.js
+calendar/julian.js
+calendar/juliandate.js
+calendar/gregorian.js
+calendar/gregoriandate.js
+numprs.js
+numfmt.js
+julianday.js
+datefmt.js
+calendar.js
+util/utils.js
+locale.js
+nfkc/all.js
+strings.js
+durfmt.js
+resources.js
+ctype.js
+localeinfo.js
+daterangefmt.js
+ctype.isalnum.js
+ctype.isalpha.js
+ctype.isascii.js
+ctype.isblank.js
+ctype.iscntrl.js
+ctype.isdigit.js
+ctype.isgraph.js
+ctype.isideo.js
+ctype.islower.js
+ctype.isprint.js
+ctype.ispunct.js
+ctype.isscript.js
+ctype.isspace.js
+ctype.isupper.js
+ctype.isxdigit.js
+scriptinfo.js
+*/
