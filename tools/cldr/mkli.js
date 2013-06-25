0a606865b8cfcc15267b7904f3f804d9b894f402
ehoogerbeets1 <ehoogerbeets1@5ac057f5-ce63-4fb3-acd1-ab13b794ca36>
2013-06-25 05:23:08 +0000

    cleaned up and jsbeautified
    git-svn-id: svn+ssh://svn.code.sf.net/p/i18nlib/code/branches/lgsi2@176 5ac057f5-ce63-4fb3-acd1-ab13b794ca36

--- tools/cldr/mkli.js
+++ tools/cldr/mkli.js
@@ -1,5 +1,5 @@
 /* 
- * mkli.js - ilib tool to remove the non-json fragments 
+ * mkli.js - ilib tool to remove the non-json fragments
  *
  * Copyright © 2013, LGE
  *
@@ -19,12 +19,9 @@
 /* 
  * This code is intended to be run under node.js
  */
-
 var fs = require('fs');
 var util = require('util');
 var common = require('./common');
-
-
 
 function usage() {
 	util.print("Usage: mkli.jss [-h] locale_data_dir\n" +
@@ -39,60 +36,54 @@ function usage() {
 localeDirName = process.argv[2];
 util.print("locale dir: " + localeDirName + "\n");
 fs.exists(localeDirName, function (exists) {
-		if (!exists) {
-			util.print("Could not access locale data directory " + localeDirName);
-			usage();
-		}
-	});
-
+	if (!exists) {
+		util.print("Could not access locale data directory " + localeDirName);
+		usage();
+	}
+});
 
 if (process.argv.length < 3) {
 	util.print('Error: not enough arguments');
 	usage();
 }
 
-var walk = function(dir) {
+function walk(dir) {
 	var results = [];
 	var list = fs.readdirSync(dir);
-	list.forEach(function(file){
+	list.forEach(function (file) {
 		file = dir + '/' + file;
 		var merged = {};
-        	var stat = fs.statSync(file);
-        	if (stat && stat.isDirectory()) (walk(file));
-	
-      		else{
-			var obj={};
-			if(file.match(/[a-z]+\.jf/)){			
-				var data = fs.readFileSync(file,'utf8');
-				try {	
-  					obj = JSON.parse(data);
-				 }
-				catch(err){
+		var stat = fs.statSync(file);
+		if (stat && stat.isDirectory())(walk(file));
+
+		else {
+			var obj = {};
+			if (file.match(/[a-z]+\.jf/)) {
+				var data = fs.readFileSync(file, 'utf8');
+				try {
+					obj = JSON.parse(data);
+				} catch (err) {
 					fs.unlink(file);
-					file='[]';
-				     }
-	
-				if(file != '[]'){
+					file = '[]';
+				}
+
+				if (file != '[]') {
 					results.push(file);
 				}
 			}
-			
-	
 		}
-	
 	});
- 
-	var merged = {};
-	for (i = 0; i < results.length; i++) {
-		var json = fs.readFileSync(results[i],'utf8');
+
+	for (var i = 0; i < results.length; i++) {
+		var json = fs.readFileSync(results[i], 'utf8');
 		var root = JSON.parse(json);
 		merged = common.merge(merged, root);
 	}
 	var path = dir;
-	if(Object.keys(merged).length!=0) {
-	    fs.writeFileSync(path + "/localeinfo.json",JSON.stringify(merged),'utf8');
-	}  
-	
+	if (Object.keys(merged).length != 0) {
+		fs.writeFileSync(path + "/localeinfo.json", JSON.stringify(merged), 'utf8');
+	}
+
 	return results;
 }
 
