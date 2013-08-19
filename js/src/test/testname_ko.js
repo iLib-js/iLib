14861418f8a7f4d896fd985f00c018ddee79c171
shashi009kant <shashi009kant@5ac057f5-ce63-4fb3-acd1-ab13b794ca36>
2013-08-19 04:36:13 +0000

    latest test cases for korean
    git-svn-id: svn+ssh://svn.code.sf.net/p/i18nlib/code/branches/lgsi-name@363 5ac057f5-ce63-4fb3-acd1-ab13b794ca36

---
+++ js/src/test/testname_ko.js
@@ -0,0 +1,348 @@
+/*
+ * testname_ko.js - test the name object in Korean
+ * 
+ * Copyright © 2013, JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function testParseSimpleName_ko_KR() {
+	var parsed = new ilib.Name("김경", {locale: 'ko-KR'},{order:"fg"});
+	assertNotUndefined(parsed);
+	
+	var expected = {
+		givenName: "경",
+		familyName: "김"
+	};
+	
+	assertObjectContains(expected, parsed);
+};
+
+function testParseTripleName_ko_KR() {
+	var parsed = new ilib.Name("김동경", {locale: 'ko-KR'});
+	assertNotUndefined(parsed);
+	
+	var expected = {
+		givenName: "동경",
+		familyName: "김"
+	};
+	
+	assertObjectContains(expected, parsed);
+};
+
+function testParseHypenatedName_ko_KR() {
+	var parsed = new ilib.Name("김동경", {locale: 'ko-KR'});
+	//console.log(parsed);
+	assertNotUndefined(parsed);
+	//console.log(parsed);
+	var expected = {
+		givenName: "동경",
+		
+		familyName: "김"
+	};
+	
+	assertObjectContains(expected, parsed);
+};
+
+function testParseQuadrupleName_ko_KR() {
+	var parsed = new ilib.Name("김동경", {locale: 'ko-KR'});
+	assertNotUndefined(parsed);
+	
+	var expected = {
+		givenName: "동경",
+		
+		familyName: "김"
+	};
+	
+	assertObjectContains(expected, parsed);
+};
+
+
+function testParseQuadrupleName_ko_KR1() {
+	var parsed = new ilib.Name("시장김동경", {locale: 'ko-KR'});
+	assertNotUndefined(parsed);
+	
+	var expected = {
+		prefix: "시장",
+		givenName: "동경",
+		
+		familyName: "김"
+	};
+	
+	assertObjectContains(expected, parsed);
+};
+
+function testParseTitle_ko_KR() {
+	var parsed = new ilib.Name("장관김동경", {locale: 'ko-KR'});
+	assertNotUndefined(parsed);
+	
+	var expected = {
+		prefix: "장관",
+		givenName: "동경",
+		familyName: "김"
+	};
+	
+	assertObjectContains(expected, parsed);
+};
+
+function testParseHonorific_ko_KR() {
+	var parsed = new ilib.Name("여왕김동경", {locale: 'ko-KR'});
+	assertNotUndefined(parsed);
+	
+	var expected = {
+		prefix: "여왕",
+		givenName: "동경",
+		familyName: "김"
+	};
+	
+	assertObjectContains(expected, parsed);
+};
+
+function testParseSuffix_ko_KR() {
+	var parsed = new ilib.Name("김동경주니어", {locale: 'ko-KR'});
+	assertNotUndefined(parsed);
+	
+	var expected = {
+		givenName: "동경",
+		familyName: "김",
+		suffix: "주니어"
+	};
+	
+	assertObjectContains(expected, parsed);
+};
+
+// for DFISH-25146
+function testParseSuffixWithComma_ko_KR() {
+	var parsed = new ilib.Name("김동경, 박사", {locale: 'ko-KR'});
+	assertNotUndefined(parsed);
+	
+	var expected = {
+		givenName: "동경",
+		familyName: "김",
+		suffix: ", 박사"
+	};
+	
+	assertObjectContains(expected, parsed);
+};
+
+
+
+function testLastNames_ko_KR() {
+	var parsed = new ilib.Name("여왕강", {locale: 'ko-KR'});
+	assertNotUndefined(parsed);
+	
+	var expected = {
+		prefix: "여왕",
+		familyName: "강"
+	};
+	
+	assertObjectContains(expected, parsed);
+};
+
+function testCompoundNames_ko_KR() {
+	var parsed = new ilib.Name("장관 김", {locale: 'ko-KR'});
+	assertNotUndefined(parsed);
+	
+	var expected = {
+		prefix: "장관",
+		familyName: " 김"
+	};
+	
+	assertObjectContains(expected, parsed);
+};
+
+
+
+/*
+ * Format tests
+ */
+
+function testFormatSimpleNameShort_ko_KR() {
+	var name = new ilib.Name({
+		prefix: "박사.",
+		givenName: "동경",
+		
+		familyName: "김",
+		suffix: ", 박사"
+	});
+	var fmt = new ilib.NameFmt({style: "short", locale: 'ko-KR'});
+	var formatted = fmt.format(name);
+	assertNotUndefined(formatted);
+	
+	var expected = "김동경";
+	
+	assertEquals(expected, formatted);
+};
+
+function testFormatSimpleNameMedium_ko_KR() {
+	var name = new ilib.Name({
+		prefix: "박사.",
+		givenName: "동경",
+		
+		familyName: "김",
+		suffix: ", 박사"
+	});
+	var fmt = new ilib.NameFmt({style: "medium", locale: 'ko-KR'});
+	var formatted = fmt.format(name);
+	assertNotUndefined(formatted);
+	
+	var expected = "김동경";
+	
+	assertEquals(expected, formatted);
+};
+
+function testFormatSimpleNameLong_ko_KR() {
+	var name = new ilib.Name({
+		prefix: "박사.",
+		givenName: "동경",
+		
+		familyName: "김",
+		suffix: ", 박사"
+	});
+	var fmt = new ilib.NameFmt({
+		style: "long", 
+		locale: 'ko-KR'
+	});
+	var formatted = fmt.format(name);
+	assertNotUndefined(formatted);
+	
+	var expected = "박사.김동경";
+	
+	assertEquals(expected, formatted);
+};
+
+function testFormatSimpleNameFull_ko_KR() {
+	var name = new ilib.Name({
+		prefix: "박사.",
+		givenName: "동경",
+		
+		familyName: "김",
+		suffix: ", 박사"
+	});
+	var fmt = new ilib.NameFmt({
+		style: "full", 
+		locale: 'ko-KR'
+	});
+	var formatted = fmt.format(name);
+	assertNotUndefined(formatted);
+	
+	var expected = "박사.김동경, 박사";
+	
+	assertEquals(expected, formatted);
+};
+
+function testFormatComplexNameShort_ko_KR() {
+	var name = new ilib.Name({
+		prefix: "여왕",
+		givenName: "경",
+		
+		familyName: "정강이",
+		suffix: "III"
+	});
+	var fmt = new ilib.NameFmt({style: "short", locale: 'ko-KR'});
+	var formatted = fmt.format(name);
+	assertNotUndefined(formatted);
+	
+	var expected = "정강이경";
+	
+	assertEquals(expected, formatted);
+};
+
+function testFormatComplexNameMedium_ko_KR() {
+	var name = new ilib.Name({
+		prefix: "여왕",
+		givenName: "동경",
+		
+		familyName: "정강이",
+		suffix: "III"
+	});
+	var fmt = new ilib.NameFmt({style: "medium", locale: 'ko-KR'});
+	var formatted = fmt.format(name);
+	assertNotUndefined(formatted);
+	
+	var expected = "정강이동경";
+	
+	assertEquals(expected, formatted);
+};
+
+function testFormatComplexNameLong_ko_KR() {
+	var name = new ilib.Name({
+		prefix: "여왕",
+		givenName: "동경",
+		
+		familyName: "정강이",
+		suffix: "III"
+	});
+	var fmt = new ilib.NameFmt({style: "long", locale: 'ko-KR'});
+	var formatted = fmt.format(name);
+	assertNotUndefined(formatted);
+	
+	var expected = "여왕정강이동경";
+	
+	assertEquals(expected, formatted);
+};
+
+function testFormatComplexNameFull_ko_KR() {
+	var name = new ilib.Name({
+		prefix: "여왕",
+		givenName: "동경",
+		
+		familyName: "정강이",
+		suffix: "III"
+	});
+	var fmt = new ilib.NameFmt({style: "full", locale: 'ko-KR'});
+	var formatted = fmt.format(name);
+	assertNotUndefined(formatted);
+	
+	var expected = "여왕정강이동경III";
+	
+	assertEquals(expected, formatted);
+};
+
+function testFormatCommasInSuffix_ko_KR() {
+	var name = new ilib.Name({
+		prefix: "여왕",
+		givenName: "동경",
+		
+		familyName: "정강이",
+		suffix: ", III, 박사"
+	});
+	var fmt = new ilib.NameFmt({style: "full", locale: 'ko-KR'});
+	var formatted = fmt.format(name);
+	assertNotUndefined(formatted);
+	
+	var expected = "여왕정강이동경, III, 박사";
+	
+	assertEquals(expected, formatted);
+};
+
+
+
+function testFormatWithNulls_ko_KR() {
+	var name = new ilib.Name({
+		prefix: null,
+		givenName: "경",
+		middleName: null,
+		familyName: "김",
+		suffix: null
+	});
+	
+	var fmt = new ilib.NameFmt({style: "long", locale: 'ko-KR'});
+	var formatted = fmt.format(name);
+	assertNotUndefined(formatted);
+	
+	var expected = "김경";
+	
+	assertEquals(expected, formatted);
+};
