4f9d0f9db652e70054bbc55d189efdc0b8578b66
birendra123 <birendra123@5ac057f5-ce63-4fb3-acd1-ab13b794ca36>
2013-08-29 06:20:52 +0000

    test cases
    git-svn-id: svn+ssh://svn.code.sf.net/p/i18nlib/code/branches/lgsi-address@424 5ac057f5-ce63-4fb3-acd1-ab13b794ca36

--- js/src/test/testaddress_BE.js
+++ js/src/test/testaddress_BE.js
@@ -21,127 +21,121 @@
 // TODO: fill in Belgian addresses here
 
 function testParseAddressNormal() {
-	var parsedAddress = new ilib.Address("X", {locale: 'nl-BE'});
+	var parsedAddress = new ilib.Address("31, Place de Brouckere\n1000 Brussels\nBelgium", {locale: 'nl-BE'});
 	
 	assertNotUndefined(parsedAddress);
-	assertEquals("Achterberglaan 23", parsedAddress.streetAddress);
-	assertEquals("Uithoorn", parsedAddress.locality);
+	assertEquals("31, Place de Brouckere", parsedAddress.streetAddress);
+	assertEquals("Brussels", parsedAddress.locality);
 	assertUndefined(parsedAddress.region);
-	assertEquals("2345 GD", parsedAddress.postalCode);
-	assertEquals("Nederland", parsedAddress.country);
+	assertEquals("1000", parsedAddress.postalCode);
+	assertEquals("Belgium", parsedAddress.country);
 	assertEquals("BE", parsedAddress.countryCode);
 };
 
-function testParseAddressNoZip() {
-	var parsedAddress = new ilib.Address("X", {locale: 'nl-BE'});
+function testParseAddressOtherNormal() {
+	var parsedAddress = new ilib.Address("31, Place de Brouckère\n1000 Bruxelles\nBelgium", {locale: 'fr-BE'});
 	
 	assertNotUndefined(parsedAddress);
-	assertEquals("Achterberglaan 23", parsedAddress.streetAddress);
-	assertEquals("Uithoorn", parsedAddress.locality);
+	assertEquals("31, Place de Brouckère", parsedAddress.streetAddress);
+	assertEquals("Bruxelles", parsedAddress.locality);
 	assertUndefined(parsedAddress.region);
-	assertEquals("Nederland", parsedAddress.country);
+	assertEquals("1000", parsedAddress.postalCode);
+	assertEquals("Belgium", parsedAddress.country);
+	assertEquals("BE", parsedAddress.countryCode);
+	
+};
+
+function testParseAddressNoZip() {
+	var parsedAddress = new ilib.Address("31, Place de Brouckère\nBruxelles\nBelgium", {locale: 'fr-BE'});
+	
+	assertNotUndefined(parsedAddress);
+	assertEquals("31, Place de Brouckère", parsedAddress.streetAddress);
+	assertEquals("Bruxelles", parsedAddress.locality);
+	assertUndefined(parsedAddress.region);
+	assertEquals("Belgium", parsedAddress.country);
 	assertEquals("BE", parsedAddress.countryCode);
 	assertUndefined(parsedAddress.postalCode);
 };
 
+
 function testParseAddressManyLines() {
-	var parsedAddress = new ilib.Address("X", {locale: 'nl-BE'});
+	var parsedAddress = new ilib.Address("31\nPlace\nde Brouckere\n1000\nBrussels\nBelgium", {locale: 'nl-BE'});
 	
 	assertNotUndefined(parsedAddress);
-	assertEquals("Claude Debussylaan 34, Vinoly Mahler 4, Toren B, 15th Floor", parsedAddress.streetAddress);
-	assertEquals("Amsterdam", parsedAddress.locality);
+	assertEquals("31, Place, de Brouckere", parsedAddress.streetAddress);
+	assertEquals("Brussels", parsedAddress.locality);
 	assertUndefined(parsedAddress.region);
-	assertEquals("1082 MD", parsedAddress.postalCode);
-	assertEquals("Nederland", parsedAddress.country);
+	assertEquals("1000", parsedAddress.postalCode);
+	assertEquals("Belgium", parsedAddress.country);
 	assertEquals("BE", parsedAddress.countryCode);
 };
 
 function testParseAddressOneLine() {
-	var parsedAddress = new ilib.Address("X", {locale: 'nl-BE'});
+	var parsedAddress = new ilib.Address("31, Place de Brouckere , 1000 Brussels , Belgium", {locale: 'nl-BE'});
 	
 	assertNotUndefined(parsedAddress);
-	assertEquals("Startbaan 16", parsedAddress.streetAddress);
-	assertEquals("Amstelveen", parsedAddress.locality);
+	assertEquals("31, Place de Brouckere", parsedAddress.streetAddress);
+	assertEquals("Brussels", parsedAddress.locality);
 	assertUndefined(parsedAddress.region);
-	assertEquals("1187 XR", parsedAddress.postalCode);
-	assertEquals("Nederland", parsedAddress.country);
+	assertEquals("1000", parsedAddress.postalCode);
+	assertEquals("Belgium", parsedAddress.country);
 	assertEquals("BE", parsedAddress.countryCode);
 };
 
-function testParseAddressSuperfluousWhitespace() {
-	var parsedAddress = new ilib.Address("X", {locale: 'nl-BE'});
-	
-	assertNotUndefined(parsedAddress);
-	assertEquals("Startbaan 16", parsedAddress.streetAddress);
-	assertEquals("Amstelveen", parsedAddress.locality);
-	assertUndefined(parsedAddress.region);
-	assertEquals("1187 XR", parsedAddress.postalCode);
-	assertEquals("Nederland", parsedAddress.country);
-	assertEquals("BE", parsedAddress.countryCode);
-};
+
 
 function testParseAddressNoDelimiters() {
-	var parsedAddress = new ilib.Address("X", {locale: 'nl-BE'});
+	var parsedAddress = new ilib.Address("31 Place de Brouckere 1000 Brussels Belgium", {locale: 'nl-BE'});
 	
 	assertNotUndefined(parsedAddress);
-	assertEquals("Startbaan 16", parsedAddress.streetAddress);
-	assertEquals("Amstelveen", parsedAddress.locality);
+	assertEquals("31 Place de Brouckere", parsedAddress.streetAddress);
+	assertEquals("Brussels", parsedAddress.locality);
 	assertUndefined(parsedAddress.region);
-	assertEquals("1187 XR", parsedAddress.postalCode);
-	assertEquals("Nederland", parsedAddress.country);
+	assertEquals("1000", parsedAddress.postalCode);
+	assertEquals("Belgium", parsedAddress.country);
 	assertEquals("BE", parsedAddress.countryCode);
 };
 
-function testParseAddressSpecialChars() {
-	var parsedAddress = new ilib.Address("X", {locale: 'nl-BE'});
-	
-	assertNotUndefined(parsedAddress);
-	assertEquals("Óók 16", parsedAddress.streetAddress);
-	assertEquals("s'Hertogen-bósch", parsedAddress.locality);
-	assertUndefined(parsedAddress.region);
-	assertEquals("1187 XR", parsedAddress.postalCode);
-	assertEquals("Nederland", parsedAddress.country);
-	assertEquals("BE", parsedAddress.countryCode);
-};
+
 
 function testParseAddressFromUS() {
-	var parsedAddress = new ilib.Address("X", {locale: 'en-US'});
+	var parsedAddress = new ilib.Address("31, Place de Brouckere , 1000 Brussels , Belgium", {locale: 'en-US'});
 	
 	// the country name is in English because this address is for a contact in a US database
 	
 	assertNotUndefined(parsedAddress);
-	assertEquals("Achterberglaan 23", parsedAddress.streetAddress);
-	assertEquals("Uithoorn", parsedAddress.locality);
+	assertEquals("31, Place de Brouckere", parsedAddress.streetAddress);
+	assertEquals("Brussels", parsedAddress.locality);
 	assertUndefined(parsedAddress.region);
-	assertEquals("2345 GD", parsedAddress.postalCode);
-	assertEquals("The Netherlands", parsedAddress.country);
+	assertEquals("1000", parsedAddress.postalCode);
+	assertEquals("Belgium", parsedAddress.country);
 	assertEquals("BE", parsedAddress.countryCode);
 };
 
 function testFormatAddress() {
 	var parsedAddress = new ilib.Address({
-		streetAddress: "Achterberglaan 23",
-		locality: "Uithoorn",
-		postalCode: "2345 GD",
-		country: "Nederland",
+		streetAddress: "31, Place de Brouckere",
+		locality: "Brussels",
+		postalCode: "1000",
+		country: "Belgium",
 		countryCode: "BE"
 	}, {locale: 'nl-BE'});
 	
-	var expected = "Achterberglaan 23\n2345 GD Uithoorn\nNederland";
+	var expected = "31, Place de Brouckere\n1000 Brussels\nBelgium";
 	var formatter = new ilib.AddressFmt({locale: 'nl-BE'});
 	assertEquals(expected, formatter.format(parsedAddress));
 };
 
 function testFormatAddressFromUS() {
 	var parsedAddress = new ilib.Address({
-		streetAddress: "Achterberglaan 23",
-		locality: "Uithoorn",
-		postalCode: "2345 GD",
-		country: "Netherlands",
+		streetAddress: "31, Place de Brouckere",
+		locality: "Brussels",
+		postalCode: "1000",
+		country: "Belgium",
 		countryCode: "BE"
 	}, {locale: 'en-US'});
 	
-	var expected = "Achterberglaan 23\n2345 GD Uithoorn\nNetherlands";
+	var expected = "31, Place de Brouckere\n1000 Brussels\nBelgium";
 	var formatter = new ilib.AddressFmt({locale: 'en-US'});
 	assertEquals(expected, formatter.format(parsedAddress));
 };
