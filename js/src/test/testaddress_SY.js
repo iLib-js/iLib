5203f0fced53f76aba3e8768e908ad60698c0a43
birendra123 <birendra123@5ac057f5-ce63-4fb3-acd1-ab13b794ca36>
2013-09-11 09:20:13 +0000

    Address format and parsing test cases
    git-svn-id: svn+ssh://svn.code.sf.net/p/i18nlib/code/branches/lgsi-address@514 5ac057f5-ce63-4fb3-acd1-ab13b794ca36

---
+++ js/src/test/testaddress_SY.js
@@ -0,0 +1,121 @@
+/*
+ * testaddress_SY.js - test the address parsing and formatting routines
+ * 
+ * Copyright © 2013, JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function testParseAddressNormal() {
+	var parsedAddress = new ilib.Address("ﺎﻠﺴﻳﺩ ﻢﺤﻣﺩ ﻊﻠﻳ 01 02/0091 ﺵﺍﺮﻋ ﻱﻮﺴﻓ ﻻ 25 06  ﺎﻟﺰﻳ ﺎﻠﻣﺩﺮﺴﻳ\n0100 ﺪﻤﺸﻗ\nﺱﻭﺮﻳﺍ", {locale: 'ar-SY'});
+	
+	assertNotUndefined(parsedAddress);
+	assertEquals("ﺎﻠﺴﻳﺩ ﻢﺤﻣﺩ ﻊﻠﻳ 01 02/0091 ﺵﺍﺮﻋ ﻱﻮﺴﻓ ﻻ 25 06  ﺎﻟﺰﻳ ﺎﻠﻣﺩﺮﺴﻳ", parsedAddress.streetAddress);
+	assertEquals("ﺪﻤﺸﻗ", parsedAddress.locality);
+	assertUndefined(parsedAddress.region);
+	assertEquals("٠١٠٠", parsedAddress.postalCode);
+	assertEquals("ﺱﻭﺮﻳﺍ", parsedAddress.country);
+	assertEquals("SY", parsedAddress.countryCode);
+};
+
+function testParseAddressNoZip() {
+	var parsedAddress = new ilib.Address("ﺎﻠﺴﻳﺩ ﻢﺤﻣﺩ ﻊﻠﻳ 01 02/0091 ﺵﺍﺮﻋ ﻱﻮﺴﻓ ﻻ 25 06  ﺎﻟﺰﻳ ﺎﻠﻣﺩﺮﺴﻳ\nﺪﻤﺸﻗ\nﺱﻭﺮﻳﺍ", {locale: 'ar-SY'});
+	
+	assertNotUndefined(parsedAddress);
+	assertEquals("ﺎﻠﺴﻳﺩ ﻢﺤﻣﺩ ﻊﻠﻳ 01 02/0091 ﺵﺍﺮﻋ ﻱﻮﺴﻓ ﻻ 25 06  ﺎﻟﺰﻳ ﺎﻠﻣﺩﺮﺴﻳ", parsedAddress.streetAddress);
+	assertEquals("ﺪﻤﺸﻗ", parsedAddress.locality);
+	assertUndefined(parsedAddress.region);
+	assertEquals("ﺱﻭﺮﻳﺍ", parsedAddress.country);
+	assertEquals("SY", parsedAddress.countryCode);
+	assertUndefined(parsedAddress.postalCode);
+};
+
+function testParseAddressNoCountry() {
+	var parsedAddress = new ilib.Address("ﺎﻠﺴﻳﺩ ﻢﺤﻣﺩ ﻊﻠﻳ 01 02/0091 ﺵﺍﺮﻋ ﻱﻮﺴﻓ ﻻ 25 06  ﺎﻟﺰﻳ ﺎﻠﻣﺩﺮﺴﻳ\n٠١٠٠\nﺪﻤﺸﻗ", {locale: 'ar-SY'});
+	
+	assertNotUndefined(parsedAddress);
+	assertEquals("ﺎﻠﺴﻳﺩ ﻢﺤﻣﺩ ﻊﻠﻳ 01 02/0091 ﺵﺍﺮﻋ ﻱﻮﺴﻓ ﻻ 25 06  ﺎﻟﺰﻳ ﺎﻠﻣﺩﺮﺴﻳ", parsedAddress.streetAddress);
+	assertEquals("ﺪﻤﺸﻗ", parsedAddress.locality);
+	assertUndefined(parsedAddress.region);
+	assertEquals("٠١٠٠", parsedAddress.postalCode);
+	assertUndefined(parsedAddress.country);
+	assertEquals("SY", parsedAddress.countryCode);
+};
+
+function testParseAddressManyLines() {
+	var parsedAddress = new ilib.Address("ﺵﺍﺮﻋ ﻱﻮﺴﻓ ﻻ 25 06  ﺎﻟﺰﻳ ﺎﻠﻣﺩﺮﺴﻳ\nﺎﻠﺴﻳﺩ ﻢﺤﻣﺩ ﻊﻠﻳ 01 02/0091\n٠١٠٠\nﺪﻤﺸﻗ\n\nﺱﻭﺮﻳﺍ\n\n\n", {locale: 'ar-SY'});
+	assertNotUndefined(parsedAddress);
+	assertEquals("ﺵﺍﺮﻋ ﻱﻮﺴﻓ ﻻ 25 06  ﺎﻟﺰﻳ ﺎﻠﻣﺩﺮﺴﻳ, ﺎﻠﺴﻳﺩ ﻢﺤﻣﺩ ﻊﻠﻳ 01 02/0091", parsedAddress.streetAddress);
+	assertEquals("ﺪﻤﺸﻗ", parsedAddress.locality);
+	assertUndefined(parsedAddress.region);
+	assertEquals("٠١٠٠", parsedAddress.postalCode);
+	assertEquals("ﺱﻭﺮﻳﺍ", parsedAddress.country);
+	assertEquals("SY", parsedAddress.countryCode);
+};
+
+
+function testParseAddressSuperfluousWhitespace() {
+	var parsedAddress = new ilib.Address("\t\t\tﺵﺍﺮﻋ ﻱﻮﺴﻓ ﻻ 25 06  ﺎﻟﺰﻳ ﺎﻠﻣﺩﺮﺴﻳ\n\n\tﺎﻠﺴﻳﺩ ﻢﺤﻣﺩ ﻊﻠﻳ 01 02/0091\n\n\t٠١٠٠\n\n\tﺪﻤﺸﻗ\n\n\tﺱﻭﺮﻳﺍ\n\n\n", {locale: 'ar-SY'});
+	
+	assertNotUndefined(parsedAddress);
+	assertEquals("ﺵﺍﺮﻋ ﻱﻮﺴﻓ ﻻ 25 06  ﺎﻟﺰﻳ ﺎﻠﻣﺩﺮﺴﻳ, ﺎﻠﺴﻳﺩ ﻢﺤﻣﺩ ﻊﻠﻳ 01 02/0091", parsedAddress.streetAddress);
+	assertEquals("ﺪﻤﺸﻗ", parsedAddress.locality);
+	assertUndefined(parsedAddress.region);
+	assertEquals("٠١٠٠", parsedAddress.postalCode);
+	assertEquals("ﺱﻭﺮﻳﺍ", parsedAddress.country);
+	assertEquals("SY", parsedAddress.countryCode);
+};
+
+
+function testParseAddressFromUS() {
+	var parsedAddress = new ilib.Address("ﺎﻠﺴﻳﺩ ﻢﺤﻣﺩ ﻊﻠﻳ 01 02/0091 ﺵﺍﺮﻋ ﻱﻮﺴﻓ ﻻ 25 06  ﺎﻟﺰﻳ ﺎﻠﻣﺩﺮﺴﻳ\n٠١٠٠\nﺪﻤﺸﻗ\nﺱﻭﺮﻳﺍ", {locale: 'en-US'});
+	
+	// the country name is in German because this address is for a contact in a German database
+	
+	assertNotUndefined(parsedAddress);
+	assertEquals("ﺎﻠﺴﻳﺩ ﻢﺤﻣﺩ ﻊﻠﻳ 01 02/0091 ﺵﺍﺮﻋ ﻱﻮﺴﻓ ﻻ 25 06  ﺎﻟﺰﻳ ﺎﻠﻣﺩﺮﺴﻳ", parsedAddress.streetAddress);
+	assertEquals("ﺪﻤﺸﻗ", parsedAddress.locality);
+	assertUndefined(parsedAddress.region);
+	assertEquals("٠١٠٠", parsedAddress.postalCode);
+	assertEquals("ﺱﻭﺮﻳﺍ", parsedAddress.country);
+	assertEquals("SY", parsedAddress.countryCode);
+};
+
+function testFormatAddress() {
+	var parsedAddress = new ilib.Address({
+		streetAddress: "ﺎﻠﺴﻳﺩ ﻢﺤﻣﺩ ﻊﻠﻳ 01 02/0091 ﺵﺍﺮﻋ ﻱﻮﺴﻓ ﻻ 25 06  ﺎﻟﺰﻳ ﺎﻠﻣﺩﺮﺴﻳ",
+		locality: "ﺪﻤﺸﻗ",
+		postalCode: "٠١٠٠",
+		country: "ﺱﻭﺮﻳﺍ",
+		countryCode: "SY"
+	}, {locale: 'ar-SY'});
+	
+	var expected = "ﺎﻠﺴﻳﺩ ﻢﺤﻣﺩ ﻊﻠﻳ 01 02/0091 ﺵﺍﺮﻋ ﻱﻮﺴﻓ ﻻ 25 06  ﺎﻟﺰﻳ ﺎﻠﻣﺩﺮﺴﻳ\n٠١٠٠\nﺪﻤﺸﻗ\nﺱﻭﺮﻳﺍ";
+	var formatter = new ilib.AddressFmt({locale: 'ar-SY'});
+	assertEquals(expected, formatter.format(parsedAddress));
+};
+
+function testFormatAddressFromUS() {
+	var parsedAddress = new ilib.Address({
+		streetAddress: "ﺎﻠﺴﻳﺩ ﻢﺤﻣﺩ ﻊﻠﻳ 01 02/0091 ﺵﺍﺮﻋ ﻱﻮﺴﻓ ﻻ 25 06  ﺎﻟﺰﻳ ﺎﻠﻣﺩﺮﺴﻳ",
+		locality: "ﺪﻤﺸﻗ",
+		postalCode: "٠١٠٠",
+		country: "ﺱﻭﺮﻳﺍ",
+		countryCode: "SY"
+	}, {locale: 'en-US'});
+	
+	var expected = "ﺎﻠﺴﻳﺩ ﻢﺤﻣﺩ ﻊﻠﻳ 01 02/0091 ﺵﺍﺮﻋ ﻱﻮﺴﻓ ﻻ 25 06  ﺎﻟﺰﻳ ﺎﻠﻣﺩﺮﺴﻳ\n٠١٠٠\nﺎﻠﻤﺴﻛﺎﺗ ﻊﻨﺑ ﻂﻴﺑ ﺎﻠﺷﺫ\nﺱﻭﺮﻳﺍ";
+	var formatter = new ilib.AddressFmt({locale: 'en-US'});
+	assertEquals(expected, formatter.format(parsedAddress));
+};
---
+++ js/src/test/testaddress_YE.js
@@ -0,0 +1,131 @@
+/*
+ * testaddress_YE.js - test the address parsing and formatting routines
+ * 
+ * Copyright © 2013, JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function testParseAddressNormal() {
+	var parsedAddress = new ilib.Address("ﺂﻗﺍی ﻢﺤﻣﺩ ﻑﻭﺍﺩ ﺂﻟ ﻑیﺽ پﺲﺗ ﻩﺍﻭ پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣\nﻉﺪﻧ\nیﻢﻧ", {locale: 'ar-YE'});
+	
+	assertNotUndefined(parsedAddress);
+	assertEquals("ﺂﻗﺍی ﻢﺤﻣﺩ ﻑﻭﺍﺩ ﺂﻟ ﻑیﺽ پﺲﺗ ﻩﺍﻭ پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣", parsedAddress.streetAddress);
+	assertEquals("ﻉﺪﻧ", parsedAddress.locality);
+	assertUndefined(parsedAddress.region);
+	assertUndefined(parsedAddress.postalCode);
+	assertEquals("یﻢﻧ", parsedAddress.country);
+	assertEquals("YE", parsedAddress.countryCode);
+};
+
+function testParseAddressNoZip() {
+	var parsedAddress = new ilib.Address("ﺂﻗﺍی ﻢﺤﻣﺩ ﻑﻭﺍﺩ ﺂﻟ ﻑیﺽ پﺲﺗ ﻩﺍﻭ پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣\nﻉﺪﻧ\nیﻢﻧ", {locale: 'ar-YE'});
+	
+	assertNotUndefined(parsedAddress);
+	assertEquals("ﺂﻗﺍی ﻢﺤﻣﺩ ﻑﻭﺍﺩ ﺂﻟ ﻑیﺽ پﺲﺗ ﻩﺍﻭ پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣", parsedAddress.streetAddress);
+	assertEquals("ﻉﺪﻧ", parsedAddress.locality);
+	assertUndefined(parsedAddress.region);
+	assertEquals("یﻢﻧ", parsedAddress.country);
+	assertEquals("YE", parsedAddress.countryCode);
+	assertUndefined(parsedAddress.postalCode);
+};
+
+function testParseAddressNoCountry() {
+	var parsedAddress = new ilib.Address("ﺂﻗﺍی ﻢﺤﻣﺩ ﻑﻭﺍﺩ ﺂﻟ ﻑیﺽ پﺲﺗ ﻩﺍﻭ پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣\n\nﻉﺪﻧ", {locale: 'ar-YE'});
+	
+	assertNotUndefined(parsedAddress);
+	assertEquals("ﺂﻗﺍی ﻢﺤﻣﺩ ﻑﻭﺍﺩ ﺂﻟ ﻑیﺽ پﺲﺗ ﻩﺍﻭ پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣", parsedAddress.streetAddress);
+	assertEquals("ﻉﺪﻧ", parsedAddress.locality);
+	assertUndefined(parsedAddress.region);
+	assertUndefined(parsedAddress.postalCode);
+	assertUndefined(parsedAddress.country);
+	assertEquals("YE", parsedAddress.countryCode);
+};
+
+function testParseAddressManyLines() {
+	var parsedAddress = new ilib.Address("پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣\nﺂﻗﺍی ﻢﺤﻣﺩ ﻑﻭﺍﺩ ﺂﻟ ﻑیﺽ پﺲﺗ ﻩﺍﻭ پﺱ ﺎﻧﺩﺍﺯ\n\nﻉﺪﻧ\n\nیﻢﻧ\n\n\n", {locale: 'ar-YE'});
+	assertNotUndefined(parsedAddress);
+	assertEquals("پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣, ﺂﻗﺍی ﻢﺤﻣﺩ ﻑﻭﺍﺩ ﺂﻟ ﻑیﺽ پﺲﺗ ﻩﺍﻭ پﺱ ﺎﻧﺩﺍﺯ", parsedAddress.streetAddress);
+	assertEquals("ﻉﺪﻧ", parsedAddress.locality);
+	assertUndefined(parsedAddress.region);
+	assertUndefined(parsedAddress.postalCode);
+	assertEquals("یﻢﻧ", parsedAddress.country);
+	assertEquals("YE", parsedAddress.countryCode);
+};
+
+function testParseAddressOneLine() {
+	var parsedAddress = new ilib.Address("پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣ , ﻡﺮﺒﻋ ﺮﻘﻣ٣٢٦٣ , ﻉﺪﻧ , یﻢﻧ", {locale: 'ar-YE'});
+	
+	assertNotUndefined(parsedAddress);
+	assertEquals("پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣, ﻡﺮﺒﻋ ﺮﻘﻣ٣٢٦٣", parsedAddress.streetAddress);
+	assertEquals("ﻉﺪﻧ", parsedAddress.locality);
+	assertUndefined(parsedAddress.region);
+	assertUndefined(parsedAddress.postalCode);
+	assertEquals("یﻢﻧ", parsedAddress.country);
+	assertEquals("YE", parsedAddress.countryCode);
+};
+
+function testParseAddressSuperfluousWhitespace() {
+	var parsedAddress = new ilib.Address("\t\t\tپﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣\n\n\tﺂﻗﺍی ﻢﺤﻣﺩ ﻑﻭﺍﺩ ﺂﻟ ﻑیﺽ پﺲﺗ ﻩﺍﻭ پﺱ ﺎﻧﺩﺍﺯ\n\n\t\n\n\tﻉﺪﻧ\n\n\tیﻢﻧ\n\n\n", {locale: 'ar-YE'});
+	
+	assertNotUndefined(parsedAddress);
+	assertEquals("پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣, ﺂﻗﺍی ﻢﺤﻣﺩ ﻑﻭﺍﺩ ﺂﻟ ﻑیﺽ پﺲﺗ ﻩﺍﻭ پﺱ ﺎﻧﺩﺍﺯ", parsedAddress.streetAddress);
+	assertEquals("ﻉﺪﻧ", parsedAddress.locality);
+	assertUndefined(parsedAddress.region);
+	assertUndefined(parsedAddress.postalCode);
+	assertEquals("یﻢﻧ", parsedAddress.country);
+	assertEquals("YE", parsedAddress.countryCode);
+};
+
+
+
+function testParseAddressFromUS() {
+	var parsedAddress = new ilib.Address("ﺂﻗﺍی ﻢﺤﻣﺩ ﻑﻭﺍﺩ ﺂﻟ ﻑیﺽ پﺲﺗ ﻩﺍﻭ پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣\n\nﻉﺪﻧ\nیﻢﻧ", {locale: 'en-US'});
+	
+	
+	assertNotUndefined(parsedAddress);
+	assertEquals("ﺂﻗﺍی ﻢﺤﻣﺩ ﻑﻭﺍﺩ ﺂﻟ ﻑیﺽ پﺲﺗ ﻩﺍﻭ پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣", parsedAddress.streetAddress);
+	assertEquals("ﻉﺪﻧ", parsedAddress.locality);
+	assertUndefined(parsedAddress.region);
+	assertUndefined(parsedAddress.postalCode);
+	assertEquals("یﻢﻧ", parsedAddress.country);
+	assertEquals("YE", parsedAddress.countryCode);
+};
+
+function testFormatAddress() {
+	var parsedAddress = new ilib.Address({
+		streetAddress: "ﺂﻗﺍی ﻢﺤﻣﺩ ﻑﻭﺍﺩ ﺂﻟ ﻑیﺽ پﺲﺗ ﻩﺍﻭ پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣",
+		locality: "ﻉﺪﻧ",
+		
+		country: "یﻢﻧ",
+		countryCode: "YE"
+	}, {locale: 'ar-YE'});
+	
+	var expected = "ﺂﻗﺍی ﻢﺤﻣﺩ ﻑﻭﺍﺩ ﺂﻟ ﻑیﺽ پﺲﺗ ﻩﺍﻭ پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣\n\nﻉﺪﻧ\nیﻢﻧ";
+	var formatter = new ilib.AddressFmt({locale: 'ar-YE'});
+	assertEquals(expected, formatter.format(parsedAddress));
+};
+
+function testFormatAddressFromUS() {
+	var parsedAddress = new ilib.Address({
+		streetAddress: "ﺂﻗﺍی ﻢﺤﻣﺩ ﻑﻭﺍﺩ ﺂﻟ ﻑیﺽ پﺲﺗ ﻩﺍﻭ پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣",
+		locality: "ﻉﺪﻧ",
+		country: "یﻢﻧ",
+		countryCode: "YE"
+	}, {locale: 'en-US'});
+	
+	var expected = "ﺂﻗﺍی ﻢﺤﻣﺩ ﻑﻭﺍﺩ ﺂﻟ ﻑیﺽ پﺲﺗ ﻩﺍﻭ پﺱ ﺎﻧﺩﺍﺯ پﺲﺗی ﺍﺮﺳﺎﻟ ﺞﻌﺒﻫ. ١٩٩٣\n\nﺎﻠﻤﺴﻛﺎﺗ ﻊﻨﺑ ﻂﻴﺑ ﺎﻠﺷﺫ\nیﻢﻧ";
+	var formatter = new ilib.AddressFmt({locale: 'en-US'});
+	assertEquals(expected, formatter.format(parsedAddress));
+};
