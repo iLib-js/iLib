5d2421e0a696ee91055b34090a0b80a6128c9ba4
gurukn <gurukn@5ac057f5-ce63-4fb3-acd1-ab13b794ca36>
2015-01-12 06:31:57 +0000

    - Test case added for parsing ChileAddress format -  Address.json modified for postal code. Modified postal code length from 8 to 7
    git-svn-id: svn+ssh://svn.code.sf.net/p/i18nlib/code/trunk@1439 5ac057f5-ce63-4fb3-acd1-ab13b794ca36

--- js/src/address/test/testaddress_CL.js
+++ js/src/address/test/testaddress_CL.js
@@ -20,19 +20,19 @@
 
 
 function testParseAddressCLNormal() {
-	var parsedAddress = new ilib.Address("Av. Bellavista N° 185, Dep. 609, 84205077, Recoleta, Chile", {locale: 'es-CL'});
+	var parsedAddress = new ilib.Address("Av. Bellavista N° 185, Dep. 609, 8420000, Recoleta, Chile", {locale: 'es-CL'});
 	
 	assertNotUndefined(parsedAddress);
 	assertEquals("Av. Bellavista N° 185, Dep. 609", parsedAddress.streetAddress);
 	assertUndefined(parsedAddress.region);
 	assertEquals("Recoleta", parsedAddress.locality);
-	assertEquals("84205077",parsedAddress.postalCode);
+	assertEquals("8420000",parsedAddress.postalCode);
 	assertEquals("Chile", parsedAddress.country);
 	assertEquals("CL", parsedAddress.countryCode);
 };
 
 function testParseAddressCLNoZip() {
-	var parsedAddress = new ilib.Address("Av. Bellavista N° 185, Dep. 609, 84205077, Recoleta, Chile", {locale: 'es-CL'});
+	var parsedAddress = new ilib.Address("Av. Bellavista N° 185, Dep. 609, 8420000, Recoleta, Chile", {locale: 'es-CL'});
 	
 	assertNotUndefined(parsedAddress);
 	assertEquals("Av. Bellavista N° 185, Dep. 609", parsedAddress.streetAddress);
@@ -40,71 +40,71 @@ function testParseAddressCLNoZip() {
 	assertEquals("Recoleta", parsedAddress.locality);
 	assertEquals("Chile", parsedAddress.country);
 	assertEquals("CL", parsedAddress.countryCode);
-	assertEquals("84205077",parsedAddress.postalCode);
+	assertEquals("8420000",parsedAddress.postalCode);
 };
 
 function testParseAddressCLManyLines() {
-	var parsedAddress = new ilib.Address("Av. Bellavista N° 185\nDep. 609\n84205077\nRecoleta\nChile", {locale: 'es-CL'});
+	var parsedAddress = new ilib.Address("Av. Bellavista N° 185\nDep. 609\n8420000\nRecoleta\nChile", {locale: 'es-CL'});
 	
 	assertNotUndefined(parsedAddress);
 	assertEquals("Av. Bellavista N° 185, Dep. 609", parsedAddress.streetAddress);
 	assertUndefined(parsedAddress.region);
 	assertEquals("Recoleta", parsedAddress.locality);
-	assertEquals("84205077",parsedAddress.postalCode);
+	assertEquals("8420000",parsedAddress.postalCode);
 	assertEquals("Chile", parsedAddress.country);
 	assertEquals("CL", parsedAddress.countryCode);
 };
 
 function testParseAddressCLOneLine() {
-	var parsedAddress = new ilib.Address("Av. Bellavista N° 185, Dep. 609, 84205077, Recoleta, Chile", {locale: 'es-CL'});
+	var parsedAddress = new ilib.Address("Av. Bellavista N° 185, Dep. 609, 8420000, Recoleta, Chile", {locale: 'es-CL'});
 	
 	assertNotUndefined(parsedAddress);
 	assertEquals("Av. Bellavista N° 185, Dep. 609", parsedAddress.streetAddress);
 	assertUndefined(parsedAddress.region);
 	assertEquals("Recoleta", parsedAddress.locality);
-	assertEquals("84205077",parsedAddress.postalCode);
+	assertEquals("8420000",parsedAddress.postalCode);
 	assertEquals("Chile", parsedAddress.country);
 	assertEquals("CL", parsedAddress.countryCode);
 };
 
 function testParseAddressCLSuperfluousWhitespace() {
-	var parsedAddress = new ilib.Address("Av. Bellavista N° 185, Dep. 609 \n\t\n 84205077\nRecoleta\t\n\n Chile  \n  \t\t\t", {locale: 'es-CL'});
+	var parsedAddress = new ilib.Address("Av. Bellavista N° 185, Dep. 609 \n\t\n 8420000\nRecoleta\t\n\n Chile  \n  \t\t\t", {locale: 'es-CL'});
 	
 	assertNotUndefined(parsedAddress);
 	assertEquals("Av. Bellavista N° 185, Dep. 609", parsedAddress.streetAddress);
 	assertUndefined(parsedAddress.region);
 	assertEquals("Recoleta", parsedAddress.locality);
-	assertEquals("84205077",parsedAddress.postalCode);
+	assertEquals("8420000",parsedAddress.postalCode);
 	assertEquals("Chile", parsedAddress.country);
 	assertEquals("CL", parsedAddress.countryCode);
 };
 
 function testParseAddressCLNoDelimiters() {
-	var parsedAddress = new ilib.Address("Av. Bellavista N° 185 Dep. 609 84205077 Recoleta Chile", {locale: 'es-CL'});
+	var parsedAddress = new ilib.Address("Av. Bellavista N° 185 Dep. 609 8420000 Recoleta Chile", {locale: 'es-CL'});
 	
 	assertNotUndefined(parsedAddress);
 	assertEquals("Av. Bellavista N° 185 Dep. 609", parsedAddress.streetAddress);
 	assertUndefined(parsedAddress.region);
 	assertEquals("Recoleta", parsedAddress.locality);
-	assertEquals("84205077",parsedAddress.postalCode);
+	assertEquals("8420000",parsedAddress.postalCode);
 	assertEquals("Chile", parsedAddress.country);
 	assertEquals("CL", parsedAddress.countryCode);
 };
 
 function testParseAddressCLSpecialChars() {
-	var parsedAddress = new ilib.Address("Av. Bellavista N° 185, Dep. 609, 84205077, Recoleta, Chile", {locale: 'es-CL'});
+	var parsedAddress = new ilib.Address("Av. Bellavista N° 185, Dep. 609, 8420000, Recoleta, Chile", {locale: 'es-CL'});
 	
 	assertNotUndefined(parsedAddress);
 	assertEquals("Av. Bellavista N° 185, Dep. 609", parsedAddress.streetAddress);
 	assertUndefined(parsedAddress.region);
 	assertEquals("Recoleta", parsedAddress.locality);
-	assertEquals("84205077",parsedAddress.postalCode);
+	assertEquals("8420000",parsedAddress.postalCode);
 	assertEquals("Chile", parsedAddress.country);
 	assertEquals("CL", parsedAddress.countryCode);
 };
 
 function testParseAddressCLFromUS() {
-	var parsedAddress = new ilib.Address("Av. Bellavista N° 185, Dep. 609, 84205077, Recoleta, Chile", {locale: 'en-US'});
+	var parsedAddress = new ilib.Address("Av. Bellavista N° 185, Dep. 609, 8420000, Recoleta, Chile", {locale: 'en-US'});
 	
 	// the country name is in English because this address is for a contact in a US database
 	
@@ -112,7 +112,7 @@ function testParseAddressCLFromUS() {
 	assertEquals("Av. Bellavista N° 185, Dep. 609", parsedAddress.streetAddress);
 	assertUndefined(parsedAddress.region);
 	assertEquals("Recoleta", parsedAddress.locality);
-	assertEquals("84205077",parsedAddress.postalCode);
+	assertEquals("8420000",parsedAddress.postalCode);
 	assertEquals("Chile", parsedAddress.country);
 	assertEquals("CL", parsedAddress.countryCode);
 };
@@ -121,12 +121,12 @@ function testFormatAddressCL() {
 	var parsedAddress = new ilib.Address({
 		streetAddress: "Av. Bellavista N° 185, Dep. 609",
 		locality: "Recoleta",
-		postalCode: "84205077",
+		postalCode: "8420000",
 		country: "Chile",
 		countryCode: "CL"
 	}, {locale: 'es-CL'});
 	
-	var expected = "Av. Bellavista N° 185, Dep. 609\n84205077\nRecoleta\nChile";
+	var expected = "Av. Bellavista N° 185, Dep. 609\n8420000\nRecoleta\nChile";
 	var formatter = new ilib.AddressFmt({locale: 'es-CL'});
 	assertEquals(expected, formatter.format(parsedAddress));
 };
@@ -135,12 +135,40 @@ function testFormatAddressCLFromUS() {
 	var parsedAddress = new ilib.Address({
 		streetAddress: "Av. Bellavista N° 185, Dep. 609",
 		locality: "Recoleta",
-		postalCode: "84205077",
+		postalCode: "8420000",
 		country: "Chile",
 		countryCode: "CL"
 	}, {locale: 'en-US'});
 	
-	var expected = "Av. Bellavista N° 185, Dep. 609\n84205077\nRecoleta\nChile";
+	var expected = "Av. Bellavista N° 185, Dep. 609\n8420000\nRecoleta\nChile";
 	var formatter = new ilib.AddressFmt({locale: 'en-US'});
 	assertEquals(expected, formatter.format(parsedAddress));
 };
+
+function testFormatAddressCL1() {
+	var parsedAddress = new ilib.Address("Señorita Patricia Vivas, Moneda 1155, 8340457, SANTIAGO, CHILE", {locale: 'es-CL'});
+	
+	// the country name is in English because this address is for a contact in a US database
+	
+	assertNotUndefined(parsedAddress);
+	assertEquals("Señorita Patricia Vivas, Moneda 1155", parsedAddress.streetAddress);
+	assertUndefined(parsedAddress.region);
+	assertEquals("SANTIAGO", parsedAddress.locality);
+	assertEquals("8340457",parsedAddress.postalCode);
+	assertEquals("CHILE", parsedAddress.country);
+	assertEquals("CL", parsedAddress.countryCode);
+};
+function testFormatAddressCL2() {
+	var parsedAddress = new ilib.Address({
+		streetAddress: "Señorita Patricia Vivas Moneda 1155",
+		locality: "SANTIAGO",
+		postalCode: "8340457",
+		country: "CHILE",
+		countryCode: "CL"
+	}, {locale: 'es-CL'});
+	
+	var expected = "Señorita Patricia Vivas Moneda 1155\n8340457\nSANTIAGO\nCHILE";
+	var formatter = new ilib.AddressFmt({locale: 'en-US'});
+	assertEquals(expected, formatter.format(parsedAddress));
+};
+
