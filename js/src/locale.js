7b1fff591add782ad8890f2b262d530e19bf9fa7
gounilee <gounilee@5ac057f5-ce63-4fb3-acd1-ab13b794ca36>
2015-04-02 10:12:05 +0000

    Update js/src/locale.js again which is related to previous commit(r1721)
    git-svn-id: svn+ssh://svn.code.sf.net/p/i18nlib/code/trunk@1722 5ac057f5-ce63-4fb3-acd1-ab13b794ca36

--- js/src/locale.js
+++ js/src/locale.js
@@ -72,7 +72,7 @@ ilib.Locale = function(language, region, variant, script) {
 		
 		if (typeof(spec) === 'string') {
 			var parts = spec.split('-');
-			parts[parts.length-1].toUpperCase();  //Fix for Android system WebView ver.40
+			parts[parts.length-1] = parts[parts.length-1].toUpperCase();  //Fix for Android system WebView ver.40
 	        for ( var i = 0; i < parts.length; i++ ) {
 	        	if (ilib.Locale._isLanguageCode(parts[i])) {
 	    			/** 
