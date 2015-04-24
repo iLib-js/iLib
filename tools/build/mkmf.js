9b1e91299f270c0efaea94bc402894633f70189b
ehoogerbeets1 <ehoogerbeets1@5ac057f5-ce63-4fb3-acd1-ab13b794ca36>
2015-04-24 09:03:49 +0000

    Some changes to get things working on Windows.
    git-svn-id: svn+ssh://svn.code.sf.net/p/i18nlib/code/branches/refactor2@1781 5ac057f5-ce63-4fb3-acd1-ab13b794ca36

--- tools/build/mkmf.js
+++ tools/build/mkmf.js
@@ -22,7 +22,7 @@
 var fs = require('fs');
 var util = require('util');
 var common = require('../cldr/common');
-var path = require('path');
+var path = require('path').posix;
 
 function usage() {
 	util.print("Usage: mkmf.js [-h] source_dir\n" +
