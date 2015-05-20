1b3275cc82934e72493db4a7014790d5e6cd3739
mok909 <mok909@5ac057f5-ce63-4fb3-acd1-ab13b794ca36>
2015-05-20 01:50:42 +0000

    Update for legacy test
    git-svn-id: svn+ssh://svn.code.sf.net/p/i18nlib/code/trunk@1845 5ac057f5-ce63-4fb3-acd1-ab13b794ca36

--- js/test/date/legacy/testdatefmt_af_ZA.js
+++ js/test/date/legacy/testdatefmt_af_ZA.js
@@ -37,7 +37,7 @@ function testDateFmtSimpleShort_af_ZA() {
 		second: 0,
 		millisecond: 0
 	});
-    assertEquals("9/29/11", fmt.format(date));
+    assertEquals("11-09-29", fmt.format(date));
 }
 
 function testDateFmtSimpleMedium_af_ZA() {
@@ -271,7 +271,7 @@ function testDateFmtSimpleLong_af_ZA() {
 		second: 0,
 		millisecond: 0
 	});
-    assertEquals("29 Sep 2011", fmt.format(date));
+    assertEquals("29 September 11", fmt.format(date));
 }
 
 function testDateFmtSimpleFull_af_ZA() {
@@ -373,7 +373,7 @@ function testDateFmtDateTimeSimpleShort_af_ZA() {
 		second: 0,
 		millisecond: 0
 	});
-    assertEquals("1:45 nm. 9/29/11", fmt.format(date));
+    assertEquals("1:45 nm. 11-09-29", fmt.format(date));
 }
 
 function testDateFmtDateTimeSimpleMedium_af_ZA() {
@@ -407,7 +407,7 @@ function testDateFmtDateTimeSimpleLong_af_ZA() {
 		second: 0,
 		millisecond: 0
 	});
-    assertEquals("1:45 nm. 29 Sep 2011", fmt.format(date));
+    assertEquals("1:45 nm. 29 September 11", fmt.format(date));
 }
 
 function testDateFmtDateTimeSimpleFull_af_ZA() {
@@ -598,7 +598,7 @@ function testDateFmtTypeDate_af_ZA() {
 		second: 0,
 		millisecond: 0
 	});
-    assertEquals("9/29/11", fmt.format(date));
+    assertEquals("11-09-29", fmt.format(date));
 }
 
 function testDateFmtTypeTime_af_ZA() {
@@ -632,7 +632,7 @@ function testDateFmtTypeDateTime_af_ZA() {
 		second: 0,
 		millisecond: 0
 	});
-    assertEquals("1:45 nm. 9/29/11", fmt.format(date));
+    assertEquals("1:45 nm. 11-09-29", fmt.format(date));
 }
 
 
@@ -768,7 +768,7 @@ function testDateFmtShortDateComponentsDMY_af_ZA() {
 		second: 0,
 		millisecond: 0
 	});
-    assertEquals("9/29/11", fmt.format(date));
+    assertEquals("11-09-29", fmt.format(date));
 }
 
 function testDateFmtShortDateComponentsWDM_af_ZA() {
