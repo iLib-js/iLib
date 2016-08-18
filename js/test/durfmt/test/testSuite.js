3e369f72b643bd6bc7383d1879542e07d82c94fe
Goun Lee <goun.lee@lge.com>
2016-08-18 15:42:11 +0900

    Fix durfmt unittest fail casea in az-Latn-AZ

--- js/test/durfmt/test/testSuite.js
+++ js/test/durfmt/test/testSuite.js
@@ -32,6 +32,7 @@ function suite() {
 		"testdurfmt.js",
 		"testdurfmt_am_ET.js",
 		"testdurfmt_ar_SA.js",
+		"testdurfmt_az_Latn_AZ.js",
 		"testdurfmt_ha_Latn_NG.js",
 		"testdurfmt_or_IN.js",
 		"testdurfmt_km_KH.js"
