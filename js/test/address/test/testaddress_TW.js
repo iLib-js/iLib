7a930184ffc0af6afc73d421f427b2ffa831d3c1
Seonghyup Park <seonghyup.park@lge.com>
2017-01-09 11:50:36 +0900

    Modify test case for zh-Hant locale of Address feature(PR#82)

--- js/test/address/test/testaddress_TW.js
+++ js/test/address/test/testaddress_TW.js
@@ -56,25 +56,25 @@ function testParseAddressTWTWLatinNoCountry() {
 };
 
 function testParseAddressTWTWAsianNormal() {
-	var parsedAddress = new Address("中華民國高雄市苓雅區802四維三路6號18樓A", {locale: 'zh-Hant-TW'});
+	var parsedAddress = new Address("台灣高雄市苓雅區802四維三路6號18樓A", {locale: 'zh-Hant-TW'});
 	
 	assertNotUndefined(parsedAddress);
 	assertEquals("苓雅區四維三路6號18樓A", parsedAddress.streetAddress);
 	assertEquals("高雄市", parsedAddress.locality);
 	assertUndefined(parsedAddress.region);
 	assertEquals("802", parsedAddress.postalCode);
-	assertEquals("中華民國", parsedAddress.country);
+	assertEquals("台灣", parsedAddress.country);
 	assertEquals("TW", parsedAddress.countryCode);
 };
 
 function testParseAddressTWTWAsianNoZip() {
-	var parsedAddress = new Address("中華民國台灣省台北市南港區經貿二路66號10樓", {locale: 'zh-Hant-TW'});
+	var parsedAddress = new Address("台灣台灣省台北市南港區經貿二路66號10樓", {locale: 'zh-Hant-TW'});
 	
 	assertNotUndefined(parsedAddress);
 	assertEquals("南港區經貿二路66號10樓", parsedAddress.streetAddress);
 	assertEquals("台北市", parsedAddress.locality);
 	assertEquals("台灣省", parsedAddress.region);
-	assertEquals("中華民國", parsedAddress.country);
+	assertEquals("台灣", parsedAddress.country);
 	assertEquals("TW", parsedAddress.countryCode);
 	assertUndefined(parsedAddress.postalCode);
 };
@@ -92,26 +92,26 @@ function testParseAddressTWTWAsianNoCountry() {
 };
 
 function testParseAddressTWTWAsianWithRegion() {
-	var parsedAddress = new Address("中華民國台灣省台高雄市苓雅區802四維三路6號18樓A", {locale: 'zh-Hant-TW'});
+	var parsedAddress = new Address("台灣台灣省台高雄市苓雅區802四維三路6號18樓A", {locale: 'zh-Hant-TW'});
 	
 	assertNotUndefined(parsedAddress);
 	assertEquals("苓雅區四維三路6號18樓A", parsedAddress.streetAddress);
 	assertEquals("台高雄市", parsedAddress.locality);
 	assertEquals("台灣省", parsedAddress.region);
 	assertEquals("802", parsedAddress.postalCode);
-	assertEquals("中華民國", parsedAddress.country);
+	assertEquals("台灣", parsedAddress.country);
 	assertEquals("TW", parsedAddress.countryCode);
 };
 
 function testParseAddressTWTWAsianZipAtEnd() {
-	var parsedAddress = new Address("中華民國\n台灣省台高雄市苓雅區四維三路6號18樓A 80245", {locale: 'zh-Hant-TW'});
+	var parsedAddress = new Address("台灣\n台灣省台高雄市苓雅區四維三路6號18樓A 80245", {locale: 'zh-Hant-TW'});
 	
 	assertNotUndefined(parsedAddress);
 	assertEquals("苓雅區四維三路6號18樓A", parsedAddress.streetAddress);
 	assertEquals("台高雄市", parsedAddress.locality);
 	assertEquals("台灣省", parsedAddress.region);
 	assertEquals("80245", parsedAddress.postalCode);
-	assertEquals("中華民國", parsedAddress.country);
+	assertEquals("台灣", parsedAddress.country);
 	assertEquals("TW", parsedAddress.countryCode);
 };
 
