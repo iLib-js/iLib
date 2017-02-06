6614ece4f030824d9ee209f69d632da6946941ee
Seonghyup Park <seonghyup.park@lge.com>
2017-02-06 09:10:47 +0900

    Modify localePage for supporting si-LK locale

--- doc/localeSpecDoc/source/app.js
+++ doc/localeSpecDoc/source/app.js
@@ -218,6 +218,7 @@ enyo.kind({
 			"Russian - Georgia":"ru-GE",
 			"Russian - Russia":"ru-RU",
 			"Russian - Ukraine":"ru-UA",
+			"Sinhalese - Sri Lanka":"si-LK",
 			"Slovak - Slovakia":"sk-SK",
 			"Slovene - Slovenia":"sl-SI",
 			"Albanian - Albania":"sq-AL", 
