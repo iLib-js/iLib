2c63419046143d4b9ed764ef00dafff5ae4b7fd9
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2017-03-22 08:54:19 -0700

    First stab at getting ilib to work with webpack.

---
+++ js/webpack.config.js
@@ -0,0 +1,38 @@
+/*
+ * webpack.config.js - configuration script for webpack
+ * 
+ * Copyright © 2017, JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = function(env) {
+    return {
+        entry: {
+            main: './lib/ilib-standard-webpack.js',
+            vendor: 'ilib'
+        },
+        output: {
+            filename: '[name].[chunkhash].js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        plugins: [
+            new webpack.optimize.CommonsChunkPlugin({
+                names: ['ilib', 'manifest']
+            })
+        ]
+    }
+};
