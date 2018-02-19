9f6ca1261d2eab16fa45c9528a1e5d83f99866bb
Edwin Hoogerbeets <edwin.hoogerbeets@healthtap.com>
2018-02-18 22:53:38 -0800

    Remove the dependency on ilib-node.js

--- js/test/calendar/nodeunit/testastro.js
+++ js/test/calendar/nodeunit/testastro.js
@@ -101,10 +101,6 @@ var testDatesAstro2 = [
     [ 2486076.5,    175.5004873603607,    956.4193104167667,    764646.5662724614,    764676.191299815],
 ];
 
-if (typeof(ilib) === "undefined") {
-    var ilib = require("../../../lib/ilib.js");
-}
-
 module.exports.testastro = {
     setUp: function(callback) {
         ilib.clearCache();
