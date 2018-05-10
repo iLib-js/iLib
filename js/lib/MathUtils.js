82ec2331e495847f122ac1f4af65abfa889ae4f4
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2018-05-10 09:27:03 -0700

    Update the copyright notices to add 2018

--- js/lib/MathUtils.js
+++ js/lib/MathUtils.js
@@ -1,7 +1,7 @@
 /*
  * MathUtils.js - Misc math utility routines
  *
- * Copyright © 2013-2015, JEDLSoft
+ * Copyright © 2013-2015, 2018 JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
