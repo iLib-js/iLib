79f998b7dab78a1d16ed27b0fcb39c1515928efe
Goun Lee <goun.lee@lge.com>
2018-07-09 05:37:49 +0430

    Make offset value as minus in order to substract correctly

--- js/lib/TimeZone.js
+++ js/lib/TimeZone.js
@@ -803,7 +803,7 @@ TimeZone.prototype.inDaylightTime = function (date, wallTime) {
 		// check if the dst property is defined -- the intrinsic JS Date object doesn't work so
 		// well if we are in the overlap time at the end of DST, so we have to work around that
 		// problem by adding in the savings ourselves
-		var offset = this.offset * 60000;
+		var offset = -(this.offset * 60000);
 		if (typeof(date.dst) !== 'undefined' && !date.dst) {
 			offset += this.dstSavings * 60000;
 		}
