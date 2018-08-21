95860821274e25723f637ca05feae6744d69b2bd
Goun Lee <goun.lee@lge.com>
2018-08-21 19:19:58 +0900

    Add task for running nodeunit test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,86 @@
 module.exports = function(grunt) {
-
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         mkdir: {
             all: {
                 options: {
-                    create: ['export', 'export/docs', 'export/js', 'export/locale', 'export/package', 'export/qt', 'export/src', 'export/tools', 'dist']
+                    create: ['export', 'export/docs', 'export/js', 'export/js/assembled', 'export/locale', 'export/package', 'export/qt', 'export/src', 'export/tools', 'dist']
+                }
+            },
+            prepare: {
+                options: {
+                    create: ['js/output', 'js/output/reports', 'js/output/js', 'js/output/dyncode', 'js/output/test', 'js/output/jsdoc']
                 }
             }
         },
+        copy: {
+            dist: {
+                files: [
+                    {cwd:"export", src: ["**/*"], dest: 'dist', expand: true}
+                ]
+            }
+        },
+        /*run_java: {
+            options: {
+
+            },
+            java_command: {
+                execOptions: {
+                    cwd: ""
+                },
+                command: "java",
+                jarName: "",
+                className: "com.ilib.tools.jsa.JSAssemble",
+                javaOptions: {
+                    "classpath":["project.classpath.jsa"]
+                },
+                javaArgs: "-l ko-KR"
+
+            }
+        },*/
+        'http-server': {
+            'dev': {
+                cwd:'js',
+                //root: '../node_modules/http-server/bin/http-server',
+                port: 9090,
+                showDir : true,
+                autoIndex: true,
+                // run in parallel with other tasks
+                runInBackground: false,
+                openBrowser: true
+            }
+        },
+        shell: {
+            mkli: {
+                command: 'cd js/data; node ../../tools/build/mkli.js'
+            },
+            compressJson: {
+
+            },
+            touch_localeinfoStamp: {
+                command:'cd js/data/locale; touch localeinfo.stamp'
+            },
+            runNodeunitAll: {
+                command: options => './js/test/runNodeunit.sh ' + options,
+            },
+            runNodeunit: {
+                command: options => './js/test/runNodeunit.sh ' + options,
+            }
+        },
+        /*run_node: {
+            geninfo: {
+                options: {
+                    cwd: 'js/data',
+                },
+                files: {src: ['tools/build.mkli.js']}
+            },
+            map:{
+                options: {
+                    cwd: 'js/test',
+                },
+                files: {src: ['js/test/maps/nodeunit/testSuite.js']}
+            },
+        },*/
         compress: {
             tgz: {
                 options: {archive: "dist/ilib.tgz"},
@@ -62,15 +134,7 @@ module.exports = function(grunt) {
                         cwd: "dist/",
                         src: ["*.{tgz,zip,jar}"],
                         dest: 'dist/checksum.md5'
-
                     }
-                ]
-            }
-        },
-        copy: {
-            dist: {
-                files: [
-                    {cwd:"export", src: ["**/*"], dest: 'dist', expand: true}
                 ]
             }
         },
@@ -79,14 +143,72 @@ module.exports = function(grunt) {
         }
   });
 
+
     grunt.loadNpmTasks('grunt-mkdir');
     grunt.loadNpmTasks('grunt-move');
+    grunt.loadNpmTasks('grunt-md5sum');
+    grunt.loadNpmTasks('grunt-shell');
+    //grunt.loadNpmTasks('grunt-run-java');
+    grunt.loadNpmTasks('grunt-http-server');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-compress');
-    grunt.loadNpmTasks('grunt-md5sum');
 
     grunt.registerTask('dist', ['compress', 'md5sum']);
+    grunt.registerTask('build', ['shell:mkli', 'shell:touch_localeinfoStamp']);
     grunt.registerTask('test', ['mkdir']);
     grunt.registerTask('default', ['test']);
+
+    grunt.registerTask('test_dynamic_uncompiled_nu_sync', ['shell:runNodeunitAll:all dynamic uncompiled sync']);
+    grunt.registerTask('test_dynamic_uncompiled_nu_async', ['shell:runNodeunitAll:all dynamic uncompiled async']);
+
+    grunt.registerTask('test_dynamic_compiled_nu_sync', ['shell:runNodeunitAll:all dynamic compiled sync']);
+    grunt.registerTask('test_dynamic_compiled_nu_async', ['shell:runNodeunitAll:all dynamic compiled async']);
+
+    grunt.registerTask('test_assembled_uncompiled_nu_sync', ['shell:runNodeunitAll:all assembled uncompiled sync']);
+    grunt.registerTask('test_assembled_uncompiled_nu_async', ['shell:runNodeunitAll:all assembled uncompiled async']);
+
+    grunt.registerTask('test_assembled_compiled_nu_sync', ['shell:runNodeunitAll:all assembled compiled sync']);
+    grunt.registerTask('test_assembled_compiled_nu_async', ['shell:runNodeunitAll:all assembled compiled async']);
+
+    grunt.registerTask('test_dyndata_uncompiled_nu_sync', ['shell:runNodeunitAll:all dynamicdata uncompiled sync']);
+    grunt.registerTask('test_dyndata_uncompiled_nu_async', ['shell:runNodeunitAll:all dynamicdata uncompiled async']);
+
+    grunt.registerTask('test_dyndata_compiled_nu_sync', ['shell:runNodeunitAll:all dynamicdata compiled sync']);
+    grunt.registerTask('test_dyndata_compiled_nu_async', ['shell:runNodeunitAll:all dynamicdata compiled async']);
+
+    grunt.registerTask('test_all_nu', ['test_dynamic_uncompiled_nu_sync','test_dynamic_uncompiled_nu_async','test_dynamic_compiled_nu_sync','test_dynamic_compiled_nu_async', 'test_assembled_uncompiled_nu_sync', 'test_assembled_uncompiled_nu_async', 'test_assembled_compiled_nu_sync', 'test_assembled_compiled_nu_async','test_dyndata_uncompiled_nu_sync', 'test_dyndata_uncompiled_nu_async', 'test_dyndata_compiled_nu_sync', 'test_dyndata_compiled_nu_async' ])
+
+    grunt.registerTask('test_address_nu_sync', ['shell:runNodeunit:sync address']);
+    grunt.registerTask('test_calendar_nu_sync', ['shell:runNodeunit:sync calendar']);
+    grunt.registerTask('test_collate_nu_sync', ['shell:runNodeunit:sync collate']);
+    grunt.registerTask('test_ctype_nu_sync', ['shell:runNodeunit:sync ctype']);
+    grunt.registerTask('test_date_nu_sync', ['shell:runNodeunit:sync date']);
+    grunt.registerTask('test_daterange_nu_sync', ['shell:runNodeunit:sync daterange']);
+    grunt.registerTask('test_durfmt_nu_sync', ['shell:runNodeunit:sync durfmt']);
+    grunt.registerTask('test_name_nu_sync', ['shell:runNodeunit:sync name']);
+    grunt.registerTask('test_number_nu_sync', ['shell:runNodeunit:sync number']);
+    grunt.registerTask('test_maps_nu_sync', ['shell:runNodeunit:sync maps']);
+    grunt.registerTask('test_phone_nu_sync', ['shell:runNodeunit:sync phone']);
+    grunt.registerTask('test_root_nu_sync', ['shell:runNodeunit:sync root']);
+    grunt.registerTask('test_strings-ext_nu_sync', ['shell:runNodeunit:sync strings-ext']);
+    grunt.registerTask('test_units_nu_sync', ['shell:runNodeunit:sync units']);
+    grunt.registerTask('test_util_nu_sync', ['shell:runNodeunit:sync util']);
+
+    grunt.registerTask('test_address_nu_async', ['shell:runNodeunit:async address']);
+    grunt.registerTask('test_calendar_nu_async', ['shell:runNodeunit:async calendar']);
+    grunt.registerTask('test_collate_nu_async', ['shell:runNodeunit:async collate']);
+    grunt.registerTask('test_ctype_nu_async', ['shell:runNodeunit:async ctype']);
+    grunt.registerTask('test_date_nu_async', ['shell:runNodeunit:async date']);
+    grunt.registerTask('test_daterange_nu_async', ['shell:runNodeunit:async daterange']);
+    grunt.registerTask('test_durfmt_nu_async', ['shell:runNodeunit:async durfmt']);
+    grunt.registerTask('test_name_nu_async', ['shell:runNodeunit:async name']);
+    grunt.registerTask('test_number_nu_async', ['shell:runNodeunit:async number']);
+    grunt.registerTask('test_maps_nu_async', ['shell:runNodeunit:async maps']);
+    grunt.registerTask('test_phone_nu_async', ['shell:runNodeunit:async phone']);
+    grunt.registerTask('test_root_nu_async', ['shell:runNodeunit:async root']);
+    grunt.registerTask('test_strings-ext_nu_async', ['shell:runNodeunit:async strings-ext']);
+    grunt.registerTask('test_units_nu_async', ['shell:runNodeunit:async units']);
+    grunt.registerTask('test_util_nu_async', ['shell:runNodeunit:async util']);
+
 };
---
+++ js/test/runNodeunit.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+echo "*** Running Nodeunit Tests ***"
+
+argc=$#
+argv0=$0
+argv1=$1
+argv2=$2
+argv3=$3
+argv4=$4
+
+run_nodeunit_tests()
+{
+    if [ 2 -eq $argc ]
+    then
+        if [ "$argv1" = "sync" ]
+        then
+            echo "argc:$argc"
+            echo "argv0:$argv0"
+            echo "argv1:$argv1"
+            echo "argv2:$argv2"
+            
+            cd js/test/$argv2/nodeunit
+            node testSuite.js
+
+            return 1
+        fi
+        if [ "$argv1" = "async" ]
+        then
+            echo "argc:$argc"
+            echo "argv0:$argv0"
+            echo "argv1:$argv1"
+            echo "argv2:$argv2"
+            
+            cd js/test/$argv2/nodeunit
+            node testSuiteAsync.js
+
+            return 1
+        fi
+    else
+        echo "The number of arguments is not satisfied."
+    fi
+
+    if [ 4 -eq $argc ]
+    then
+        if [ "$argv1" = "all" ]
+        then
+            echo "argc:$argc"
+            echo "argv0:$argv0"
+            echo "argv1:$argv1"
+            echo "argv2:$argv2"
+            echo "argv3:$argv3"
+            echo "argv4:$argv4"
+            cd js/test
+            node testRunner.js $argv2 $argv3 all $argv4
+
+            return 1
+        fi
+    else
+        echo "The number of arguments is not satisfied."
+    fi
+}
+
+run_nodeunit_tests
+
+exit 0
+
