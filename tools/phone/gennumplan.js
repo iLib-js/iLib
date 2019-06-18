21b639cb9b954d0eb51dde3298d941a7e19001e7
Goun Lee <goun.lee@lge.com>
2019-06-18 17:45:48 +0900

    More updates in gennumplan.js

--- tools/phone/gennumplan.js
+++ tools/phone/gennumplan.js
@@ -47,23 +47,56 @@ var metadata = require("./metadata.json");
 var countryData = metadata.countries;
 var country, filename, exampleNums = {};
 
-for (country in countryData) {
-	var numPlanData = {};
-	filename = path.join(toDir, 'und', country)
-	if (!fs.existsSync(filename)) {
-		mkdirs(filename);
-	}
-	numPlanData["region"] = country;
-	numPlanData["countryCode"] = countryData[country]["phone_code"];
-	numPlanData["iddCode"] = countryData[country]["idd_prefix"];
-	numPlanData["trunkCode"] = countryData[country]["national_prefix"];
-	
-	exampleNums[country] = countryData[country]["examples"];
+function getFormatChars(phonedata) {
+    var formats = phonedata.formats;
+    if (!formats) return "";
 
-	var file1 = path.join(filename, "numplan.json");
-	console.log("Creating " + filename + "/numplan.json")
-	fs.writeFileSync(file1, JSON.stringify(numPlanData, true, 4), "utf-8");
+    var numformats = formats.length;
+    var combineString = "", uniqChars = "";
+    var i;
+
+    for (i=0; i< numformats; i++) {
+        combineString += formats[i].format;
+    }
+
+    trimString = combineString.replace(/[\$[0-9]|[0-9]*/g, "").replace(/\s+/g," ");
+
+    if (trimString.length >1) {
+        for (i=0; i < trimString.length; i++) {
+            if (uniqChars.indexOf(trimString[i]) == -1) {
+                uniqChars += trimString[i];
+            }
+        }
+    }
+    return uniqChars ? uniqChars : trimString;
 }
+
+for (country in countryData) {
+    var numPlanData = {};
+    var regx = RegExp(/[\!|\?|\:]/);
+
+    filename = path.join(toDir, 'und', country)
+    if (!fs.existsSync(filename)) {
+        mkdirs(filename);
+    }
+    numPlanData["region"] = country;
+    numPlanData["countryCode"] = countryData[country]["phone_code"];
+
+    if (regx.test(countryData[country]["idd_prefix"])){
+        numPlanData["iddCode"] = "It's regular expression. Need to be checked";
+    } else {
+        numPlanData["iddCode"] = countryData[country]["idd_prefix"];
+    }
+
+    numPlanData["trunkCode"] = countryData[country]["national_prefix"];
+    numPlanData["commonFormatChars"] = getFormatChars(countryData[country]);
+
+    exampleNums[country] = countryData[country]["examples"];
+
+    var file1 = path.join(filename, "numplan.json");
+    console.log("Creating " + filename + "/numplan.json")
+    fs.writeFileSync(file1, JSON.stringify(numPlanData, true, 4), "utf-8");
+}
+
 var file2 = path.join(toDir, "exampleNums.json");
 fs.writeFileSync(file2, JSON.stringify(exampleNums, true, 4), "utf-8");
-
