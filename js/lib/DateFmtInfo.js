840ec57e556335dbebb7feb20fe9ea4cb5ed6295
Edwin Hoogerbeets <ehoogerbeets@box.com>
2019-06-24 16:59:25 -0700

    Fixed dateformtinfo unit tests to be truly async
    The tests were not testing async properly. Now they are.

--- js/lib/DateFmtInfo.js
+++ js/lib/DateFmtInfo.js
@@ -51,7 +51,8 @@ var ISet = require("./ISet.js");
  * instance works
  */
 var DateFmtInfo = function(options) {
-    if (!options || options.sync) {
+    var sync = typeof(options.sync) === "boolean" ? options.sync : true;
+    if (!options || sync) {
         this.fmt = new DateFmt(options);
         this._init(options);
     } else {
