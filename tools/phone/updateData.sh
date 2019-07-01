44c892a558808fb08b0168fb26a599f6d28cbfa0
Goun Lee <goun.lee@lge.com>
2019-07-01 16:02:34 +0900

    Add updateData.sh script to sync up medata automatically

---
+++ tools/phone/updateData.sh
@@ -0,0 +1,49 @@
+#! /bin/sh
+
+echo "Running updateData.sh..."
+echo "(1): Update Metadata from libphonenumber-js"
+echo "(2): Generate numplans.json files"
+echo "(3): Generate phonefmt.json files"
+echo "(4): Remove libphonenumber-js directory"
+echo "(5): Remove temp directory"
+echo "Enter the task number you want to do...: "
+
+read -p "Selected number is ..." number
+
+tasks()
+{
+    if [ $number = "1" ];
+    then
+        if [ ! -d "libphonenumber-js" ] 
+        then
+            echo "libphonenumer-js doesn't exist!"
+            git clone https://github.com/catamphetamine/libphonenumber-js.git
+            sh ./libphonenumber-js/autoupdate.sh 
+        else
+            cd libphonenumber-js
+            git pull
+            sh ./autoupdate.sh
+        fi
+    elif [ $number = "2" ]
+    then
+        echo "Executing genplan.js ..."
+        node gennumplan.js
+    elif [ $number = 3 ]
+    then
+        echo "Executing genphonefmt.js ..."
+        node genphonefmt.js
+        elif [ $number = 4 ]
+    then
+        echo "Executing genphonefmt.js ..."
+        node genphonefmt.js
+        elif [ $number = 5 ]
+    then
+        echo "Executing genphonefmt.js ..."
+        node genphonefmt.js
+    else
+        echo "Re-type the proper number"
+    fi
+}
+
+tasks
+exit 0
