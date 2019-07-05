5403d80edc5af0a4dc9b33dc3b99c750f0579e4c
Goun Lee <goun.lee@lge.com>
2019-07-05 17:06:16 +0900

    Add gengeoinfo.js script to get the area number information

--- tools/phone/updateData.sh
+++ tools/phone/updateData.sh
@@ -2,9 +2,9 @@
 
 echo "Running updateData.sh..."
 echo "(1): Update Metadata from libphonenumber-js"
-echo "(2): Generate numplans.json files"
-echo "(3): Generate phonefmt.json files"
-echo "(4): Remove libphonenumber-js directory"
+echo "(2): Get a geo info from libphonenumber"
+echo "(3): Generate JSON data related PhoneNumber information ..."
+echo "(4): Remove libphonenumber-js/ and libphonenumber/ directory"
 echo "(5): Remove temp directory"
 echo "Enter the task number you want to do...: "
 
@@ -24,20 +24,29 @@ tasks()
             sh ./autoupdate.sh
         fi
     elif [ $number = "2" ];then
+        if [ ! -d "libphonenumber" ];then
+            echo "libphonenumer-js doesn't exist!"
+            git clone https://github.com/google/libphonenumber.git
+        else
+            cd libphonenumber
+            git pull
+        fi
+    elif [ $number = "3" ];then
         echo "Executing gennumplan.js ..."
         node gennumplan.js
-    elif [ $number = 3 ];then
         echo "Executing genphonefmt.js ..."
         node genphonefmt.js
+        echo "Executing gengeoinfo.js ..."
+        node gengeoinfo.js
     elif [ $number = 4 ];then
+        echo "Removing libphonenumber directory ..."
+        if [ -d "libphonenumber-js" ];then
+            rm -rf libphonenumber
+        fi
+    elif [ $number = 5 ];then
         echo "Removing libphonenumber-js directory ..."
         if [ -d "libphonenumber-js" ];then
             rm -rf libphonenumber-js
-        fi
-    elif [ $number = 5 ];then
-        echo "Removing tmp directory ..."
-        if [ -d "libphonenumber-js" ];then
-            rm -rf tmp
         fi
     else
         echo "Re-type the proper number"
