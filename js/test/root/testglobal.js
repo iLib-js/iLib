9e86ba7ec690c8df008e0d81b62ecef2909482db
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2019-10-28 11:41:55 -0700

    Update the unit test for versions

--- js/test/root/testglobal.js
+++ js/test/root/testglobal.js
@@ -74,7 +74,7 @@ module.exports.testglobal = {
             return;
         }
         test.expect(1);
-        test.equal(ilib.getVersion().substring(0,4), "14.4");
+        test.equal(ilib.getVersion().substring(0,4), "14.5");
         test.done();
     },
     
