c15a45644e31b4c6af287220a36b848775784dbe
Goun Lee <gouni6020@gmail.com>
2020-08-27 12:47:19 +0900

    Add testcase to test native digits

--- js/test/number/testnumfmt.js
+++ js/test/number/testnumfmt.js
@@ -1,7 +1,7 @@
 /*
  * testnumfmt.js - test the number formatter object
  *
- * Copyright © 2012-2018 JEDLSoft
+ * Copyright © 2012-2018 2020 JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -6869,6 +6869,97 @@ module.exports.testnumfmt = {
         test.ok(fmt);
 
         test.equal(fmt.format(12345678900), "1.23E+10");
+        test.done();
+    },
+
+    //test cases for my-MM
+
+    testNumFmt_my_MM: function(test) {
+        test.expect(2);
+        var fmt = new NumFmt({
+            locale: "my-MM",
+            maxFractionDigits: 2
+        });
+        test.ok(fmt);
+        test.equal(fmt.format(12340568.78), "၁၂,၃၄၀,၅၆၈.၇၈");
+        test.done();
+    },
+    testNumFmtNative_my_MM: function(test) {
+        test.expect(2);
+        var fmt = new NumFmt({
+            locale: "my-MM",
+            useNative: true,
+            maxFractionDigits: 2
+        });
+        test.ok(fmt);
+        test.equal(fmt.format(-123.57), "-၁၂၃.၅၇");
+        test.done();
+    },
+    testNumFmtCurrencyFormatNativeCurrencyForLocale_my_MM1: function(test) {
+        test.expect(2);
+        var fmt = new NumFmt({
+            type: "currency",
+            locale: "my-MM",
+            useNative: true,
+            currency: "MMK"
+        });
+        test.ok(fmt);
+        test.equal(fmt.format(123.57), "၁၂၄ K");
+        test.done();
+    },
+    testNumFmtCurrencyFormatNegativeNativeCurrencyForLocale_my_MM1: function(test) {
+        test.expect(2);
+        var fmt = new NumFmt({
+            type: "currency",
+            locale: "my-MM",
+            useNative: true,
+            currency: "MMK"
+        });
+        test.ok(fmt);
+        test.equal(fmt.format(-123.57), "-၁၂၄ K");
+        test.done();
+    },
+    testNumFmtCurrencyFormatCorrectCurrencyForLocale_my_MM: function(test) {
+        test.expect(2);
+        var fmt = new NumFmt({
+            type: "currency",
+            locale: "my-MM",
+            currency: "MMK"
+        });
+        test.ok(fmt);
+        test.equal(fmt.format(123.57), "၁၂၄ K");
+        test.done();
+    },
+    testNumFmtPercentageFormatRegular_my_MM: function(test) {
+        test.expect(2);
+        var fmt = new NumFmt({
+            locale: "my-MM",
+            useNative: true,
+            type: "percentage"
+        });
+        test.ok(fmt);
+        test.equal(fmt.format(57.8), "੫੭.੮%");
+        test.done();
+    },
+    testNumFmtPercentageFormatRegular_my_MM: function(test) {
+        test.expect(2);
+        var fmt = new NumFmt({
+            locale: "my-MM",
+            useNative: true,
+            type: "percentage"
+        });
+        test.ok(fmt);
+        test.equal(fmt.format(-57.8), '-၅၇.၈%');
+        test.done();
+    },
+    testNumFmtPercentageNativeFormatRegular_my_MM: function(test) {
+        test.expect(2);
+        var fmt = new NumFmt({
+            locale: "my-MM",
+            type: "percentage"
+        });
+        test.ok(fmt);
+        test.equal(fmt.format(-57.8), '-၅၇.၈%');
         test.done();
     }
 };
