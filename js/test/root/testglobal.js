fb3dc690bc311d5ca21f9ff0ad0c5608125ed209
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2021-01-24 14:23:01 -0800

    Fix version unit test

--- js/test/root/testglobal.js
+++ js/test/root/testglobal.js
@@ -74,7 +74,7 @@ module.exports.testglobal = {
             return;
         }
         test.expect(1);
-        test.equal(ilib.getVersion().substring(0,4), "14.6");
+        test.equal(ilib.getVersion().substring(0,4), "14.7");
         test.done();
     },
     
