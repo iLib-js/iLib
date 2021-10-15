d8e7206870476f380e7188b84ba994b298bcc33e
Goun Lee <goun.lee@lge.com>
2021-10-15 15:39:49 +0900

    Update copyright year

--- js/lib/ResBundle.js
+++ js/lib/ResBundle.js
@@ -1,7 +1,7 @@
 /*
  * ResBundle.js - Resource bundle definition
  *
- * Copyright © 2012-2016, 2018-2019, JEDLSoft
+ * Copyright © 2012-2016, 2018-2019, 2021 JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
