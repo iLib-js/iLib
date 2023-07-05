29b85a334a6874b30f0a881a18fb67241149d19d
Goun Lee <goun.lee@lge.com>
2023-07-05 10:21:00 +0900

    Update file's copyright year

--- js/test/root/testresourcesasync.js
+++ js/test/root/testresourcesasync.js
@@ -1,7 +1,7 @@
 /*
  * testresourcesasync.js - test the Resources object
  *
- * Copyright © 2018-2019, 2022 JEDLSoft
+ * Copyright © 2018-2019, 2022-2023 JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
