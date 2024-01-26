138ed0ee754d0e4cbe684bf8211efc2f2b02650c
Goun Lee <goun.lee>
2024-01-26 10:51:10 +0900

    Update delimiters.jf to cldr v44.1

--- js/test/root/testlocaleinfo.js
+++ js/test/root/testlocaleinfo.js
@@ -1,7 +1,7 @@
 /*
  * testlocaleinfo.js - test the locale info object
  *
- * Copyright © 2012-2017, 2020-2023 JEDLSoft
+ * Copyright © 2012-2017, 2020-2024 JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -11744,8 +11744,8 @@ module.exports.testlocaleinfo = {
         var info = new LocaleInfo("es-ES");
         test.ok(info !== null);
 
-        test.equal(info.getDelimiterQuotationStart(), "«");
-        test.equal(info.getDelimiterQuotationEnd(), "»");
+        test.equal(info.getDelimiterQuotationStart(), "“");
+        test.equal(info.getDelimiterQuotationEnd(), "”");
         test.done();
     },
     testLocaleInfoQuotation_zh_CN: function(test) {
@@ -11998,8 +11998,8 @@ module.exports.testlocaleinfo = {
         var info = new LocaleInfo("tg-TJ");
         test.ok(info !== null);
 
-        test.equal(info.getDelimiterQuotationStart(), "“");
-        test.equal(info.getDelimiterQuotationEnd(), "”");
+        test.equal(info.getDelimiterQuotationStart(), "»");
+        test.equal(info.getDelimiterQuotationEnd(), "«");
         test.equal(info.getPaperSize(), "A4");
         test.done();
     },
