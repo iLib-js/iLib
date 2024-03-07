f8f641dd53ca98ae5cead4fc0929a203d02e4514
Goun Lee <goun.lee@lge.com>
2024-03-07 15:37:42 +0900

    Update the build.xmld to generate ilib-standard-flutter.js

---
+++ js/lib/ilib-standard-flutter-inc.js
@@ -0,0 +1,99 @@
+/**
+ * @license
+ * Copyright © 2024, JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * ilib-full-inc.js - metafile that includes all other js files
+ */
+
+/* !depends
+ilib.js
+DateRngFmt.js
+IDate.js
+MathUtils.js
+SearchUtils.js
+DateFactory.js
+Astro.js
+Calendar.js
+CalendarFactory.js
+RataDie.js
+JulianDay.js
+HebrewDate.js
+HebrewCal.js
+IslamicCal.js
+IslamicDate.js
+JulianCal.js
+JulianDate.js
+GregorianCal.js
+GregorianDate.js
+GregRataDie.js
+ThaiSolarCal.js
+ThaiSolarDate.js
+PersRataDie.js
+PersianCal.js
+PersianDate.js
+PersianAlgoCal.js
+PersianAlgoDate.js
+HanCal.js
+HanDate.js
+EthiopicCal.js
+EthiopicDate.js
+CopticCal.js
+CopticDate.js
+INumber.js
+NumFmt.js
+DateFmt.js
+Utils.js
+Locale.js
+IString.js
+ISet.js
+DurationFmt.js
+ResBundle.js
+CType.js
+LocaleInfo.js
+DateRngFmt.js
+isAlnum.js
+isAlpha.js
+isAscii.js
+isBlank.js
+isCntrl.js
+isDigit.js
+isGraph.js
+isIdeo.js
+isLower.js
+isPrint.js
+isPunct.js
+isSpace.js
+isUpper.js
+isXdigit.js
+isScript.js
+ScriptInfo.js
+LocaleMatcher.js
+GlyphString.js
+NormString.js
+CaseMapper.js
+TimeZone.js
+Charset.js
+Charmap.js
+CharmapFactory.js
+CharmapTable.js
+UTF8.js
+UTF16BE.js
+UTF16LE.js
+Country.js
+ListFmt.js
+*/
