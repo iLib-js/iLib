17606ed1f2d537abc07597f6e8e5d8eb9745157d
seonmi1.jin <seonmi1.jin@lge.com>
2026-04-28 09:59:12 +0900

    Update scripts.jf to CLDR v48.2

--- tools/cldr/genlangscripts.js
+++ tools/cldr/genlangscripts.js
@@ -1,7 +1,7 @@
 /*
  * genlangscripts.js - ilib tool to generate the json data about ISO 15924 scripts
  *
- * Copyright © 2013-2018, 2020-2022, 2024 JEDLSoft
+ * Copyright © 2013-2018, 2020-2022, 2024, 2026 JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -106,15 +106,7 @@ for (var language in scripts) {
         if (!fs.existsSync(filename)) {
             fs.mkdirSync(filename);
         }
-        // special cases where we disagree with CLDR
-        if (language === 'az' || language === 'ms' || language === 'kk' || language === 'pa'|| language === 'tk'|| language === 'ha' || language === 'uz') {
-            scripts[language] = scripts[language].reverse();
-        } else if (language == 'ky'|| language == 'tg') {
-            var lang = scripts[language];
-            var tmp = lang[0];
-            lang[0] = lang[1];
-            lang[1] = tmp;
-        }
+
         console.log(language + ':\t"scripts": ' + JSON.stringify(scripts[language]) + ',');
         scripts_name["scripts"] = scripts[language];
         scripts_name.generated = true;
